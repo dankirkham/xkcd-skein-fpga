fe13223291af7d66e8ac4861e8b511f156976975
Dan <daniel.a.kirkham@gmail.com>
2015-01-13 01:17:07 -0600

    Completed input register

--- src/input_register.v
+++ src/input_register.v
@@ -1,7 +1,30 @@
 module input_register (
 	input clk_i,
-	input rst_i,
-	input [1023:0] output_register_i,
-	output 
+	input zero_i,
+	input write_i,
+	input [1023:0] state_i,
 	
+	output [1023:0] state_o
+	);
+	
+reg [1023:0] state_d, state_q;
+
+assign state_o = state_q;
+
+always @(*) begin
+	if (write_i) begin
+		state_d = state_i;
+	end else begin
+		state_d = state_q;
+	end
+end
+
+always @(posedge clk) begin
+	state_q <= state_d;
+	
+	if (zero_i) begin
+		state_q <= 64'd0;
+	end
+end
+
 endmodule
