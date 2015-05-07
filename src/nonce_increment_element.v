e52bb51a3e0c79356791ae4125af04fa02d41c5e
Dan <daniel.a.kirkham@gmail.com>
2015-05-07 09:23:59 -0500

    Changed design of nonce_increment_register to meet timing constraints.

--- src/nonce_increment_element.v
+++ src/nonce_increment_element.v
@@ -1,18 +1,17 @@
 module nonce_increment_element (
 	input [7:0] byte_i,
 	input carry_i,
-	output reg [7:0] byte_o,
-	output reg carry_o
+	output [7:0] byte_o,
+	output carry_o
 );
 
-always @(*) begin // 33 to 127
-	if ((byte_i + carry_i) < 8'd127) begin
-		byte_o = byte_i + carry_i;
-		carry_o = 1'b0;
-	end else begin
-		byte_o = byte_i + carry_i - 8'd94;
-		carry_o = 1'b1;
-	end
+reg [6:0] sum;
+
+assign byte_o = {1'b1, sum[5:0], 1'b0};
+assign carry_o = sum[6];
+
+always @(*) begin
+	sum[6:0] = byte_i[6:1] + 1;
 end
 
 endmodule
