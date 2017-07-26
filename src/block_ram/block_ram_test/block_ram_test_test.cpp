8ff29857597774311c4e237d82275dc6ce99e2e5
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2017-07-26 06:19:40 -0500

    Updated block_ram_test with TX Busy test and Bad Header test.

--- src/block_ram/block_ram_test/block_ram_test_test.cpp
+++ src/block_ram/block_ram_test/block_ram_test_test.cpp
@@ -18,9 +18,8 @@ void edge(Vblock_ram_test* top) {
   top->clk_i ^= 1;
 }
 
-int main(int argc, char **argv, char **env) {
-  Verilated::commandArgs(argc, argv);
-
+void read_write_test() {
+  cout << " * Read/Write Test" << endl;
   Vblock_ram_test* top = new Vblock_ram_test;
 
   top->clk_i = 0;
@@ -112,29 +111,94 @@ int main(int argc, char **argv, char **env) {
   _assert("New TX Data bit SHOULD be set", top->new_tx_data_o != 0);
   _assert("TX Data should be the header constant", top->tx_byte_o == 0x9A);
 
+  // TX Busy Wait
+  top->rx_byte_i = 0x00;
+  top->new_rx_data_i = 0;
+  top->tx_busy_i = 1;
+  for (int i = 0; i < 8; i++) {
+    edge(top);
+    edge(top);
+  }
+
   // Receive Data Byte Low
   top->rx_byte_i = 0x00;
   top->new_rx_data_i = 0;
+  top->tx_busy_i = 0;
   edge(top);
   edge(top);
 
   _assert("Write bit should not be set", top->write_o == 0);
   _assert("New TX Data bit SHOULD be set", top->new_tx_data_o != 0);
-  cout << std::to_string(top->tx_byte_o) << endl;
   _assert("TX Data should be valid", top->tx_byte_o == 0xAD);
+
+  // TX Busy Wait
+  top->rx_byte_i = 0x00;
+  top->new_rx_data_i = 0;
+  top->tx_busy_i = 1;
+  for (int i = 0; i < 8; i++) {
+    edge(top);
+    edge(top);
+  }
 
   // Receive Data Byte High
   top->rx_byte_i = 0x00;
   top->new_rx_data_i = 0;
+  top->tx_busy_i = 0;
   edge(top);
   edge(top);
 
   _assert("Write bit should not be set", top->write_o == 0);
   _assert("New TX Data bit SHOULD be set", top->new_tx_data_o != 0);
-  cout << std::to_string(top->tx_byte_o) << endl;
   _assert("TX Data should be valid", top->tx_byte_o == 0xDE);
 
   delete top;
+}
+
+void bad_header_test() {
+  cout << " * Bad Header Test" << endl;
+  Vblock_ram_test* top = new Vblock_ram_test;
+
+  top->clk_i = 0;
+  top->tx_busy_i = 0;
+
+  // Send Incorrect Header Byte
+  top->rx_byte_i = 0xD4;
+  top->new_rx_data_i = 1;
+  edge(top);
+  edge(top);
+
+  // Send Address Byte
+  top->rx_byte_i = 0x42;
+  top->new_rx_data_i = 1;
+  edge(top);
+  edge(top);
+
+  // Provide data
+  top->data_i = 0xDEAD;
+
+  // Send Read Byte
+  top->rx_byte_i = 0x00;
+  top->new_rx_data_i = 1;
+  edge(top);
+  edge(top);
+
+  // Receive Header
+  top->rx_byte_i = 0x00;
+  top->new_rx_data_i = 0;
+  edge(top);
+  edge(top);
+
+  _assert("New TX Data bit should NOT be set", top->new_tx_data_o == 0);
+  _assert("TX Data should NOT be the header constant", top->tx_byte_o != 0x9A);
+
+  delete top;
+}
+
+int main(int argc, char **argv, char **env) {
+  Verilated::commandArgs(argc, argv);
+
+  read_write_test();
+  bad_header_test();
 
   exit(0);
 }
