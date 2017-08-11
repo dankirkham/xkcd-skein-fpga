ef76a0fee59147379bac3d000f5950efb1050eeb
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2017-08-11 07:33:12 -0500

    Added nonce test.

---
+++ src/core_sim/test/nonce_test.asm
@@ -0,0 +1,14 @@
+// CoreSimInput 4645649
+SelectCore // Select the core
+
+// CoreSimInput 15235
+Constant 0
+RotateLeft 48 0
+Read 0
+// CoreSimAssert 15235
+
+// CoreSimInput 15235
+Nonce 0
+RotateLeft 48 1
+Read 1
+// CoreSimAssert 15235
