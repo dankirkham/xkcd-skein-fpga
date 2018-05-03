ccb72f413b4447465e763ebcb2db707f34e055a3
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2018-05-03 06:52:42 -0500

    Added flag for clang 5.0.0

--- src/module.mk
+++ src/module.mk
@@ -5,7 +5,7 @@ run: obj_dir/V$(MODULE)
 
 obj_dir/V$(MODULE): $(MODULE).v $(MODULE)_test.cpp $(LIB_DIR)/test.cpp
 	mkdir -p obj_dir
-	g++ -c $(LIB_DIR)/test.cpp -I$(LIB_DIR) -o obj_dir/test.o
+	clang++ -std=c++11 -c $(LIB_DIR)/test.cpp -I$(LIB_DIR) -o obj_dir/test.o
 	verilator -Wall --cc $(MODULE).v --exe test.o $(MODULE)_test.cpp $(addprefix -I$(SRC_DIR)/,$(DEPENDENCIES)) $(VERILATOR_FLAGS) -CFLAGS -I../$(LIB_DIR)
 	make -C obj_dir -j -f V$(MODULE).mk V$(MODULE)
 
