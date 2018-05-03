5ca3f33b10a33972a9886575694821fcd148dabf
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2018-05-03 06:59:03 -0500

    Added compiler flag for verilator side.

--- src/module.mk
+++ src/module.mk
@@ -6,7 +6,7 @@ run: obj_dir/V$(MODULE)
 obj_dir/V$(MODULE): $(MODULE).v $(MODULE)_test.cpp $(LIB_DIR)/test.cpp
 	mkdir -p obj_dir
 	clang++ -std=c++11 -c $(LIB_DIR)/test.cpp -I$(LIB_DIR) -o obj_dir/test.o
-	verilator -Wall --cc $(MODULE).v --exe test.o $(MODULE)_test.cpp $(addprefix -I$(SRC_DIR)/,$(DEPENDENCIES)) $(VERILATOR_FLAGS) -CFLAGS -I../$(LIB_DIR)
+	verilator -Wall --cc $(MODULE).v --exe test.o $(MODULE)_test.cpp $(addprefix -I$(SRC_DIR)/,$(DEPENDENCIES)) $(VERILATOR_FLAGS) -CFLAGS -I../$(LIB_DIR) -CFLAGS -std=c++11
 	make -C obj_dir -j -f V$(MODULE).mk V$(MODULE)
 
 .PHONY: clean
