9327d4a6f076147e303c79a55ea9b149f6b08be8
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2018-05-09 21:30:06 -0500

    Switch to GCC on Travis.

--- src/module.mk
+++ src/module.mk
@@ -5,7 +5,7 @@ run: obj_dir/V$(MODULE)
 
 obj_dir/V$(MODULE): $(MODULE).v $(MODULE)_test.cpp $(LIB_DIR)/test.cpp
 	mkdir -p obj_dir
-	clang++ -c $(LIB_DIR)/test.cpp -I$(LIB_DIR) -o obj_dir/test.o
+	$(CC) -c $(LIB_DIR)/test.cpp -I$(LIB_DIR) -o obj_dir/test.o
 	verilator -Wall --cc $(MODULE).v --exe test.o $(MODULE)_test.cpp $(addprefix -I$(SRC_DIR)/,$(DEPENDENCIES)) $(VERILATOR_FLAGS) -CFLAGS -I../$(LIB_DIR)
 	make -C obj_dir -j -f V$(MODULE).mk V$(MODULE)
 
