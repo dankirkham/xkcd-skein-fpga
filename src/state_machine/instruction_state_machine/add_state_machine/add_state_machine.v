f92328c84f488d71e715519b890d3939c9223536
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2019-06-15 09:20:36 -0500

    Added add state machine.

---
+++ src/state_machine/instruction_state_machine/add_state_machine/add_state_machine.v
@@ -0,0 +1,30 @@
+module add_state_machine (
+  input start_i,
+  output reg [20:0] instruction_o,
+  output done_o
+);
+
+wire [20:0] instruction_w;
+
+assign instruction_w = { 1'b0, 1'b0, 8'b0, 2'b0, 1'b0, 1'b0, 4'h8, 3'd0 };
+// assign instruction_w = {
+//     1'b0, // save_core_selection
+//     1'b0, // ram_write_bit
+//     address_w, // address
+//     2'b0, // input_select
+//     output_select_w, // output_select
+//     output_enable_w, // output_enable
+//     4'hC, // alu_opcode
+//     3'd0, // global_command
+// };
+
+assign done_o = start_i;
+
+always @(*) begin
+  if (start_i)
+    instruction_o = instruction_w;
+  else
+    instruction_o = 21'bZ;
+end
+
+endmodule
